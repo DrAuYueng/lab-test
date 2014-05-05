0d2439bbd71a56b4441d405933b4d0592ed1718f
ouyang <ouyang1204@gmail.com>
2014-05-05 16:49:58 +0800

    查看keystore证书库证书，了解其内部存储结构

---
+++ lab-test/src/lab/keystore/KeyStoreTest.java
@@ -0,0 +1,48 @@
+package lab.keystore;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.util.Enumeration;
+
+public class KeyStoreTest {
+
+	public static void main(String[] args) {
+		String password = "changeit";
+		
+		String filepath = System.getenv("JAVA_HOME") +"\\jre\\lib\\security\\cacerts";
+		try {
+			KeyStore ks = KeyStore.getInstance(KeyStore.getDefaultType());
+			InputStream stream = new FileInputStream(new File(filepath));
+			ks.load(stream, password.toCharArray());
+			Enumeration<String> aliases = ks.aliases();
+			while (aliases.hasMoreElements()) {
+				String alias = aliases.nextElement();
+				System.out.println("alias:"+alias);
+			}
+		} catch (KeyStoreException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (CertificateException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
