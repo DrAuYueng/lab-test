67a594e34df8f7781a436ee2446f21dbc4d25634
ouyang <ouyang1204@gmail.com>
2014-05-06 09:09:22 +0800

    httpClient test

---
+++ lab-test/src/lab/httpclient/HttpClientTest.java
@@ -0,0 +1,19 @@
+package lab.httpclient;
+
+import java.security.KeyStore;
+
+import javax.net.ssl.SSLContext;
+
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLSocketFactory;
+
+public class HttpClientTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// new
+		// SSLSocketFactory(KeyStore.getInstance(KeyStore.getDefaultType()));
+		// new SSLConnectionSocketFactory(SSLContext.getInstance("TLS"));
+	}
+
+}
